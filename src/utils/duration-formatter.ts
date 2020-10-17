131dbe1bbebd818e8557af7c35be52c626e6a8d0
Kelly <kelly.farlow@gmail.com>
2020-10-17 18:49:09 +1000

    Apply linting and formatting fixes

--- src/utils/duration-formatter.ts
+++ src/utils/duration-formatter.ts
@@ -1,8 +1,10 @@
-import * as moment from 'moment'
+import * as moment from 'moment';
 
-export function formatDuration(durationInSeconds: number, includeSeconds: boolean = true) {
-    const duration = moment.duration(durationInSeconds, 'seconds')
-    let formattedStr = `${duration.hours()}h ${duration.minutes()}m`
-    if (includeSeconds) {formattedStr = `${formattedStr}  ${duration.seconds()}s`}
-    return formattedStr
+export function formatDuration(durationInSeconds: number, includeSeconds = true): string {
+  const duration = moment.duration(durationInSeconds, 'seconds');
+  let formattedStr = `${duration.hours()}h ${duration.minutes()}m`;
+  if (includeSeconds) {
+    formattedStr = `${formattedStr} ${duration.seconds()}s`;
+  }
+  return formattedStr;
 }
